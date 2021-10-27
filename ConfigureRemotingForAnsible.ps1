b2fa5f7b749854c906ab4304ee9c1d8d0ccd13c5
Aleksandr Rulev <arulev@zxz.su>
2021-10-27 17:56:30 +0300

    update

--- ConfigureRemotingForAnsible.ps1
+++ ConfigureRemotingForAnsible.ps1
@@ -360,48 +360,48 @@ Else
     }
 }
 
-# Check for basic authentication.
-$basicAuthSetting = Get-ChildItem WSMan:\localhost\Service\Auth | Where-Object {$_.Name -eq "Basic"}
+# # Check for basic authentication.
+# $basicAuthSetting = Get-ChildItem WSMan:\localhost\Service\Auth | Where-Object {$_.Name -eq "Basic"}
 
-If ($DisableBasicAuth)
-{
-    If (($basicAuthSetting.Value) -eq $true)
-    {
-        Write-Verbose "Disabling basic auth support."
-        Set-Item -Path "WSMan:\localhost\Service\Auth\Basic" -Value $false
-        Write-Log "Disabled basic auth support."
-    }
-    Else
-    {
-        Write-Verbose "Basic auth is already disabled."
-    }
-}
-Else
-{
-    If (($basicAuthSetting.Value) -eq $false)
-    {
-        Write-Verbose "Enabling basic auth support."
-        Set-Item -Path "WSMan:\localhost\Service\Auth\Basic" -Value $true
-        Write-Log "Enabled basic auth support."
-    }
-    Else
-    {
-        Write-Verbose "Basic auth is already enabled."
-    }
-}
+# If ($DisableBasicAuth)
+# {
+#     If (($basicAuthSetting.Value) -eq $true)
+#     {
+#         Write-Verbose "Disabling basic auth support."
+#         Set-Item -Path "WSMan:\localhost\Service\Auth\Basic" -Value $false
+#         Write-Log "Disabled basic auth support."
+#     }
+#     Else
+#     {
+#         Write-Verbose "Basic auth is already disabled."
+#     }
+# }
+# Else
+# {
+#     If (($basicAuthSetting.Value) -eq $false)
+#     {
+#         Write-Verbose "Enabling basic auth support."
+#         Set-Item -Path "WSMan:\localhost\Service\Auth\Basic" -Value $true
+#         Write-Log "Enabled basic auth support."
+#     }
+#     Else
+#     {
+#         Write-Verbose "Basic auth is already enabled."
+#     }
+# }
 
 # If EnableCredSSP if set to true
-If ($EnableCredSSP)
-{
-    # Check for CredSSP authentication
-    $credsspAuthSetting = Get-ChildItem WSMan:\localhost\Service\Auth | Where-Object {$_.Name -eq "CredSSP"}
-    If (($credsspAuthSetting.Value) -eq $false)
-    {
-        Write-Verbose "Enabling CredSSP auth support."
-        Enable-WSManCredSSP -role server -Force
-        Write-Log "Enabled CredSSP auth support."
-    }
-}
+# If ($EnableCredSSP)
+# {
+#     # Check for CredSSP authentication
+#     $credsspAuthSetting = Get-ChildItem WSMan:\localhost\Service\Auth | Where-Object {$_.Name -eq "CredSSP"}
+#     If (($credsspAuthSetting.Value) -eq $false)
+#     {
+#         Write-Verbose "Enabling CredSSP auth support."
+#         Enable-WSManCredSSP -role server -Force
+#         Write-Log "Enabled CredSSP auth support."
+#     }
+# }
 
 # If ($GlobalHttpFirewallAccess) {
 #     Enable-GlobalHttpFirewallAccess
